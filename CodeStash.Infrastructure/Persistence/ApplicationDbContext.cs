4a155aca27b46a984c5877b6f3d59aa0e6611ea9
Ibrahim Suleiman <ebeeraheem@gmail.com>
2025-01-07 09:17:15 +0100

    feat: add user and role and run initial migration

--- CodeStash.Infrastructure/Persistence/ApplicationDbContext.cs
+++ CodeStash.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using CodeStash.Core;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace CodeStash.Infrastructure.Persistence;
 public class ApplicationDbContext(DbContextOptions options) : IdentityDbContext(options)
 {
+    public required DbSet<ApplicationUser> ApplicationUsers { get; set; }
+    public required DbSet<ApplicationRole> ApplicationRoles { get; set; }
 }
