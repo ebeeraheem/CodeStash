12adf90fc8f90e923bf46ef1da3199bf89d8c6a9
Ibrahim Suleiman <ebeeraheem@gmail.com>
2025-01-13 23:24:25 +0100

    feat: add tag controller

---
+++ CodeStash.API/Controllers/TagsController.cs
@@ -0,0 +1,45 @@
+﻿using CodeStash.Application.Models;
+using CodeStash.Application.Services;
+using CodeStash.Core.Dtos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodeStash.API.Controllers;
+
+[Authorize(Roles = Roles.Admin)]
+[Route("api/[controller]")]
+[ApiController]
+public class TagsController(ITagService tagService) : ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> AddTag(TagDto request)
+    {
+        var result = await tagService.AddTagAsync(request);
+
+        return result.IsSuccess ? Ok(result) : BadRequest(result);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateTag(Guid id, TagDto request)
+    {
+        var result = await tagService.UpdateTagAsync(id, request);
+
+        return result.IsSuccess ? Ok(result) : BadRequest(result);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteTag(Guid id)
+    {
+        var result = await tagService.DeleteTagAsync(id);
+
+        return result.IsSuccess ? Ok(result) : BadRequest(result);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetTags()
+    {
+        var result = await tagService.GetAllTags();
+
+        return Ok(result);
+    }
+}
