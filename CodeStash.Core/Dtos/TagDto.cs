45d1d88a31c9491b5de31572da798cc077144c8e
Ibrahim Suleiman <ebeeraheem@gmail.com>
2025-01-15 19:35:44 +0100

    fix: add max length of 50 for tag name

--- CodeStash.Core/Dtos/TagDto.cs
+++ CodeStash.Core/Dtos/TagDto.cs
@@ -1,5 +1,8 @@
-﻿namespace CodeStash.Core.Dtos;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CodeStash.Core.Dtos;
 public class TagDto
 {
+    [MaxLength(50)]
     public required string Name { get; set; }
 }
