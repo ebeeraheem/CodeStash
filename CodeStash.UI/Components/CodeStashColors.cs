8f80f08cfc7178b9a6bcd2df8137c7ca8cdb8e90
Ibrahim Suleiman <ebeeraheem@gmail.com>
2025-02-06 16:52:40 +0100

    feat: add new colours component to visualize colours

--- CodeStash.UI/Components/CodeStashColors.cs
+++ CodeStash.UI/Components/CodeStashColors.cs
@@ -2,10 +2,10 @@
 
 public static class CodeStashColors
 {
-    public static string Primary = "#B5F8FE";
-    public static string Secondary = "0081AF";
-    public static string Tertiary = "#34D399";
-    public static string Background = "#02182B";
-    public static string AppbarBackground = "#02182B";
-    public static string DrawerBackground = "#02182B";
+    public const string Primary = "#B5F8FE";
+    public const string Secondary = "#0081AF";
+    public const string Tertiary = "#34D399";
+    public const string Background = "#02182B";
+    public const string AppbarBackground = "#02182B";
+    public const string DrawerBackground = "#02182B";
 }
