2e73f65cd3604df4e517c6ea56f61d92bb76077f
Ibrahim Suleiman <ebeeraheem@gmail.com>
2025-02-06 16:42:54 +0100

    feat: configure default theme colours

---
+++ CodeStash.UI/Components/CodeStashColors.cs
@@ -0,0 +1,11 @@
+﻿namespace CodeStash.UI.Components;
+
+public static class CodeStashColors
+{
+    public static string Primary = "#B5F8FE";
+    public static string Secondary = "0081AF";
+    public static string Tertiary = "#34D399";
+    public static string Background = "#02182B";
+    public static string AppbarBackground = "#02182B";
+    public static string DrawerBackground = "#02182B";
+}
