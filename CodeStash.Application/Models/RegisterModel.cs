44fad3995dc67183516da4ab621a487510048da6
Ibrahim Suleiman <ebeeraheem@gmail.com>
2025-02-09 09:59:03 +0100

    feat: implement register page with validation

--- CodeStash.Application/Models/RegisterModel.cs
+++ CodeStash.Application/Models/RegisterModel.cs
@@ -6,10 +6,10 @@ public class RegisterModel
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
 
-    [Length(minimumLength: 3, maximumLength: 50)]
+    [Length(minimumLength: 3, maximumLength: 50, ErrorMessage = "Username must be between 3 and 50 characters long")]
     public required string UserName { get; set; }
 
-    [EmailAddress]
+    [EmailAddress(ErrorMessage = "Invalid email address")]
     public required string Email { get; set; }
 
     [DataType(DataType.Password)]
